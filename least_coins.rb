1a71983fcfbec748fed584afbe1784d60454d8f0
christinasherpa <chrisher24@charter.newvisions.org>
2018-08-08 20:18:22 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,23 @@
 #write out your code here
-
+ puts "This code will give you the least amount of coins!"
 def least_coins(cents)
-
-#Code your answer here!
+  
+  #Code your answer here!
+  #cents = cents.get.chomp
+  remainder = cents 
+  
+  quarter = cents / 25
+  remainder =  remainder % 25 
+  dime = remainder / 10
+  remainder = remainder % 10
+  nickel = remainder / 5
+  remainder = remainder % 5
+  penny = remainder / 1
+  remainder = remainder % 1
+  
+  
+  return total_coins = {:quarters => quarter, :dimes => dime, :nickels => nickel, :pennies => penny}
 
 end
+
+least_coins (93)
